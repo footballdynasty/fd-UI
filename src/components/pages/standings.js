c770216f851f75a3941884ed6696c8b9ead50a66
brodycritchlow <brodycritchlow@gmail.com>
2024-06-19 22:31:41 -0600

    Add table for standings

--- src/components/pages/standings.js
+++ src/components/pages/standings.js
@@ -1,9 +1,70 @@
+import React, { useState, useEffect } from 'react';
+import { Table, TableBody, TableContainer, TableHead, TableRow, Paper, Container} from '@mui/material';
+import { styled } from '@mui/material/styles';
+import TableCell, { tableCellClasses } from '@mui/material/TableCell';
+
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: "#1976d2",
+    color: theme.palette.common.white,
+  },
+  [`&.${tableCellClasses.body}`]: {
+    fontSize: 14,
+  },
+}));
+
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+  '&:nth-of-type(odd)': {
+    backgroundColor: theme.palette.action.hover,
+  },
+  // hide last border
+  '&:last-child td, &:last-child th': {
+    border: 0,
+  },
+}));
+
 function Standings() {
-    return (
-      <div>
-        <h1>Standings Page</h1>
-      </div>
-    );
+  const [standings, setStandings] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:8080/api/v1.0/standings')
+      .then(response => response.json())
+      .then(data => { 
+        setStandings(data);
+        console.log(data);
+      })
+      .catch(error => console.error(error));
+  }, []);
+
+  return (
+    <Container fixed sx={{ pt: '2em' }}>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650, maxWidth:1550 }} aria-label="standings table">
+          <TableHead>
+            <StyledTableRow>
+              <StyledTableCell>Team</StyledTableCell>
+              <StyledTableCell align="right">Wins</StyledTableCell>
+              <StyledTableCell align="right">Losses</StyledTableCell>
+            </StyledTableRow>
+          </TableHead>
+          <TableBody>
+            {standings.map((standing) => (
+              <StyledTableRow
+                key={standing.team.id}
+                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+              >
+                <StyledTableCell component="th" scope="row">
+                  {standing.team.name}
+                </StyledTableCell>
+                <StyledTableCell align="right">{standing.wins}</StyledTableCell>
+                <StyledTableCell align="right">{standing.losses}</StyledTableCell>
+              </StyledTableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Container>
+  );
 }
 
 export default Standings;
